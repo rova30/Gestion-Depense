d9525f50637b0c12fc76379c7e4b537354dbffa8
rova30 <rovarobel30@gmail.com>
2023-07-26 15:09:44 +0300

    ao

--- mobile-ionic/src/data/login.service.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { baseUrl } from './global_config';
-export const loginService = (login:string,mdp:string) => {
-    const loginData = {login: login, mdp: mdp};
-    return axios.post(baseUrl('login'), loginData);
-};
