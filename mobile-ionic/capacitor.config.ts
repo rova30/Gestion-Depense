b174dede4fc92856a7728ff20c0bcb3909983676
rova30 <rovarobel30@gmail.com>
2023-07-26 18:23:35 +0300

    android

--- mobile-ionic/capacitor.config.ts
+++ mobile-ionic/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'mobile-ionic',
-  webDir: 'build',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
