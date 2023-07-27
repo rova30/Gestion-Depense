a930eb10e69163ef7cf112807bc689b00546c626
rova30 <rovarobel30@gmail.com>
2023-07-27 22:18:08 +0300

    udpdate maivana

--- mobile-ionic/vite.config.ts
+++ mobile-ionic/vite.config.ts
@@ -1,16 +1,30 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+plugins: [react()],
+optimizeDeps: {
+esbuildOptions: {
+// Node.js global to browser globalThis
+define: {
+global: 'globalThis',
+},
+},
+},
+build: {
+rollupOptions: {
+output: {
+manualChunks(id) {
+if (id.includes('node_modules')) {
+return id
+.toString()
+.split('node_modules/')[1]
+.split('/')[0]
+.toString();
+}
+},
+},
+},
+},
+});
